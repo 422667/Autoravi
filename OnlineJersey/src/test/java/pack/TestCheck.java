8c7d076aa841daba26f21c98619aae73304bbde1
admin <admin@admin-PC>
2016-12-01 19:45:32 +0530

    CG

---
+++ OnlineJersey/src/test/java/pack/TestCheck.java
@@ -0,0 +1,16 @@
+package pack;
+
+import org.testng.annotations.Test;
+
+
+public class TestCheck {
+	@Test
+	public void repair()
+	{
+		System.out.println("Ravindra kumar automation tester");
+	}
+	
+	
+	
+	
+}
